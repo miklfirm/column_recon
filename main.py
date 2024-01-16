0bcc578f9345cf25d79dec3f6e62040a3c111e10
Miklushov N. <miklfirm@bk.ru>
2024-01-16 12:57:05 +0300

    Pandas without error low_memory

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import pandas as pd
+from dask import dataframe as dd
 
 filtered_df = pd.DataFrame()
-df_original = pd.read_csv("original_list.txt", header=None)
-df_search = pd.read_csv("search_list.txt", header=None)
+df_original = pd.read_csv("original_list.txt", on_bad_lines='skip', header=None)
+df_search = pd.read_csv("search_list.txt", on_bad_lines='skip', header=None)
 
 
-# print(df_original[0])
-for i in range(0,len(df_search)):
+for i in range(0, len(df_search)):
     """
-    Function create new DataFrame with finded words in original_list from search_list 
+    Function create new DataFrame with finded words in original_list from search_list
     """
     filtered_df_temp = df_original.loc[df_original[0].str.contains(df_search[0][i])]
     filtered_df = filtered_df._append(filtered_df_temp)
-
-# print(filtered_df)
 
 filtered_df.to_csv("output_list.txt", index=False)
 
--- temp.py
+++ temp.py
@@ -1,21 +1,9 @@
-# Importing pandas as pd
+import dask.dataframe as dd
 import pandas as pd
 
-# Creating the first Dataframe using dictionary
-df1 = df = pd.DataFrame({"a": [1, 2, 3, 4],
-                         "b": [5, 6, 7, 8]})
+df = dd.read_csv("original_list.txt")
+print(df.head())
+# dff = pd.read_csv("original_list.txt")
 
-# Creating the Second Dataframe using dictionary
-df2 = pd.DataFrame({"a": [1, 2, 3],
-                    "b": [5, 6, 7]})
-
-# Print df1
-print("Printing df1")
-print(df1, "\n")
-
-print("Printing df1 append")
-newdf = pd.DataFrame()
-newdf = newdf._append(df1)
-newdf = newdf._append(df1)
-
-print(newdf, "\n")
+# n_rows = df.shape[0].compute()
+# print(df.head())
